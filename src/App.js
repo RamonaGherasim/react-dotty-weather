c37fc278f0bc1426f2b363236ba0abe1f83c65e9
RamonaGherasim <79341158+RamonaGherasim@users.noreply.github.com>
2021-05-23 00:38:14 +0100

    Moved footer to App component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import WeatherApp from "./WeatherApp";
-
-
+import Footer from "./Footer";
 
 function App() {
   return (
     <div className="App">
   <WeatherApp />
+  <div>
+    <Footer />
+    </div>
     </div>
   );
 }
