bd6c6114eda5800c42c30cf13ac40dd573f45800
RamonaGherasim <79341158+RamonaGherasim@users.noreply.github.com>
2021-06-08 20:00:30 +0100

    created a load function

--- src/Forecast.js
+++ src/Forecast.js
@@ -17,24 +17,30 @@ function handleResponse (response) {
   setLoaded (true);
 }
 
+function load () {
+   let apiKey="5eabfe88b69ebff8b2d2c1968bc189ae";
+  let latitude = props.coordinates.lat;
+  let longitude = props.coordinates.lon;
+  let apiUrl=`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`
+    
+  axios.get(apiUrl).then(handleResponse);
+}
+
 if (loaded) {
   return (
     <div className="Forecast">
       {forecast.map(function(dailyForecast, index) {
        if (index < 5) {
        return ( <ForecastDay data={dailyForecast} />)
+       } else {
+         return null;
        }
       })}
     </div>
     )
 } else {
-  let apiKey="5eabfe88b69ebff8b2d2c1968bc189ae";
-  let latitude = props.coordinates.lat;
-  let longitude = props.coordinates.lon;
-  let apiUrl=`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`
-    
-  axios.get(apiUrl).then(handleResponse);
- 
-  return null;
+load ()
+
+return null;
 }
 }
